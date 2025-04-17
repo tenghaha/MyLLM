3ba9e81efdd372742981613d5a2a7458fdfa61c2
tenghaha <tengyuntao_23@sina.com>
2025-04-17 20:52:01 +0800

    重构workflow

--- llm/state_graph.py
+++ llm/state_graph.py
@@ -6,8 +6,10 @@ from typing_extensions import Annotated, TypedDict
 from langgraph.graph import START, END, MessagesState, StateGraph
 from langgraph.graph.message import add_messages
 from langgraph.prebuilt import ToolNode, tools_condition
+from langchain.tools.retriever import create_retriever_tool
 from langchain_core.messages import BaseMessage
 from langchain_core.runnables import RunnableConfig
+from langchain_core.vectorstores import VectorStoreRetriever
 from langchain_deepseek import ChatDeepSeek
 from langchain.prompts import ChatPromptTemplate, MessagesPlaceholder
 from langchain_community.tools.tavily_search import TavilySearchResults
@@ -17,6 +19,7 @@ from langchain_community.tools.tavily_search import TavilySearchResults
 class State(TypedDict):
     messages: Annotated[Sequence[BaseMessage], add_messages]
     system_prompt: str
+    retriever: VectorStoreRetriever
 
 class ConfigSchema(TypedDict):
     model: str
@@ -26,49 +29,63 @@ class ConfigSchema(TypedDict):
     frequency_penalty: float
     presence_penalty: float
 
-# 工具节点：搜索功能
-search_tool = TavilySearchResults(name="search_tool", max_results=5)
-
-# 主agent
-def agent(state: State, config: RunnableConfig):
-    model = ChatDeepSeek(
-        # base_url=BASE_URL[st.session_state.model_config["api"]],
-        model_name=config["configurable"].get("model"),
-        max_tokens=config["configurable"].get("max_tokens"),
-        temperature=config["configurable"].get("temperature"),
-        top_p=config["configurable"].get("top_p"),
-        frequency_penalty=config["configurable"].get("frequency_penalty"),
-        presence_penalty=config["configurable"].get("presence_penalty")
-    )
-    # 决定是否call search_tool
-    if config["configurable"].get("use_web_context"):
-        tool_choice = "auto"
-    else:
-        tool_choice = "none"
-    model = model.bind_tools(
-        [search_tool],
-        tool_choice=tool_choice
-    )
-    prompt_template = ChatPromptTemplate.from_messages(
-        [
-            ("system", "{system_prompt}"),
-            MessagesPlaceholder(variable_name="messages"),
-        ]
-    )
-    if method := config["configurable"].get("response_type") == "json_mode":
-        chain = prompt_template | model.with_structured_output(method)
-        response = {"role": "ai", "content": json.dumps(chain.invoke(state), ensure_ascii=False)}
-    else:
-        chain = prompt_template | model
-        response = chain.invoke(state)
-    return {"messages": [response]}
-
 
 class WorkFlow(StateGraph):
-    def __init__(self):
+    def __init__(self, retriever: VectorStoreRetriever | None = None):
         super().__init__(state_schema=State, config_schema=ConfigSchema)
+        self.search_tool = TavilySearchResults(
+            name="search_tool",
+            description="Searching webpages to provide user with an informed response. ",
+            max_results=5
+        )    # 工具节点：搜索功能
+        self.retriever_tool = None
+        if retriever:
+            self.retriever_tool = create_retriever_tool(
+                retriever,
+                name="retriever_tool",
+                description="Analyze the contents of the user's uploaded documents to extract pertinent information."
+            )
+        self._build()
+
+    
+    # 主agent
+    def _agent(self, state: State, config: RunnableConfig):
+        model = ChatDeepSeek(
+            # base_url=BASE_URL[st.session_state.model_config["api"]],
+            model_name=config["configurable"].get("model"),
+            max_tokens=config["configurable"].get("max_tokens"),
+            temperature=config["configurable"].get("temperature"),
+            top_p=config["configurable"].get("top_p"),
+            frequency_penalty=config["configurable"].get("frequency_penalty"),
+            presence_penalty=config["configurable"].get("presence_penalty")
+        )
+        # 决定是否call search_tool
+        if config["configurable"].get("use_web_context"):
+            tool_choice = "auto"
+        else:
+            tool_choice = "none"
+        model = model.bind_tools(
+            [self.search_tool],
+            tool_choice=tool_choice
+        )
+        prompt_template = ChatPromptTemplate.from_messages(
+            [
+                ("system", "{system_prompt}"),
+                MessagesPlaceholder(variable_name="messages"),
+            ]
+        )
+        if method := config["configurable"].get("response_type") == "json_mode":
+            chain = prompt_template | model.with_structured_output(method)
+            response = {"role": "ai", "content": json.dumps(chain.invoke(state), ensure_ascii=False)}
+        else:
+            chain = prompt_template | model
+            response = chain.invoke(state)
+        return {"messages": [response]}
+    
+    # 构建计算图
+    def _build(self):
         self.add_edge(START, "agent")
-        self.add_node("agent", agent)
+        self.add_node("agent", self._agent)
         self.add_conditional_edges(
             "agent",
             tools_condition,
@@ -77,5 +94,5 @@ class WorkFlow(StateGraph):
                 END: END
             }
         )
-        self.add_node("search", ToolNode([search_tool]))
+        self.add_node("search", ToolNode([self.search_tool]))
         self.add_edge("search", "agent")
